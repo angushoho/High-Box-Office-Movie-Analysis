2a3644a824a3654538e4d49ac8ac6ec75e948192
angus <angus2313@gmail.com>
2018-08-06 23:12:26 +0800

    first edition

--- get_top250.py
+++ get_top250.py
@@ -1,6 +1,6 @@
 from requests_html import HTMLSession
 import requests
-
+import json
 
 
 ''' get ranking list '''
@@ -13,22 +13,36 @@ rating_list = []
 for title in titles:
     rating_list.append(title.text)
 
+
 ''' get box office '''
+
+
 api_key = '2455a527'
 year = ''
 plot = 'full'
 
 IMDb_rating = []
+rotten_tomato_rating = []
+json_data = {}
 for title in rating_list:
     url = 'http://www.omdbapi.com/?t=' + title + '&y=' + year + 'plot=' + plot + '&apikey=' + api_key
     r = requests.get(url)
+    # print(r.text)
+    data = json.loads(r.text)
     try:
-        data = json.loads(r.text)
         score = data['Ratings'][0]['Value']
-        score = str(score).split('/')[0]
+        score = score.split('/')[0]
         score = float(score)
         IMDb_rating.append(score)
     except Exception as e:
         IMDb_rating.append(None)
 
-print(IMDb_rating)
+    try:
+        score_r = data['Ratings'][1]['Value']
+        score_r = score_r.split('%')[0]
+        score_r = float(score_r)
+        rotten_tomato_rating.append(score_r)
+    except Exception as e:
+        rotten_tomato_rating.append(None)
+
+print(rotten_tomato_rating)
